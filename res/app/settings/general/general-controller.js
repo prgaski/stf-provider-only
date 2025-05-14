792a5bbbaa73bdc55b53b926c3942462eff4b98e
Patrick Gaskin <pg7254@outlook.com>
2025-05-13 22:06:59 -0400

    remove most non-provider files

--- res/app/settings/general/general-controller.js
+++
@@ -1,9 +0,0 @@
-/**
-*  Copyright © 2024 contains code contributed by Orange SA, authors: Denis Barbaron - Licensed under the Apache license 2.0
-**/
-
-module.exports = function GeneralCtrl($scope, AppState) {
-  $scope.isAdmin = function() {
-    return AppState.user.privilege === 'admin'
-  }
-}
